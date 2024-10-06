748b0ec63f9f1b7ce603a3aa9c8750721e04f4c5
maviem <mukawum@gmail.com>
2024-10-06 22:12:01 +0200

    allure report fixed

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
+
+
+
 
 /**
  * Read environment variables from file.
@@ -22,14 +26,27 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: [['list'],
+  // ['html'],
+  // ['allure-playwright',{outputFolder: 'results'}]],
+
+
+  reporter: [
+    ['list'],                       // Optional: Display test results in console
+    ['allure-playwright']           // Allure report generation
+  ],
+  
+
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',                  // Optional: Capture traces for each test
+    screenshot: 'on',               // Capture screenshots on failure
+    video: 'on', 
   },
 
   /* Configure projects for major browsers */
@@ -76,4 +93,8 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+ 
 });
+
+
--- tests/ticketsPOM.spec.ts
+++ tests/ticketsPOM.spec.ts
@@ -5,6 +5,8 @@ import { BuyerDetailsPage } from '../pages/buyerdetails.page';
 import { ServicesPage } from '../pages/services.page';
 import { PaymentPage } from '../pages/payment.page';
 
+
+
 test('Buying Billetto Tickets',async ({page}) => {
     test.slow();
     const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -15,6 +17,7 @@ test('Buying Billetto Tickets',async ({page}) => {
     const Ticket_Services = new ServicesPage(page);
     const selectPayment = new PaymentPage(page)
 
+   
     await NewTicket.gotoTicket();
 
 
@@ -22,6 +25,8 @@ test('Buying Billetto Tickets',async ({page}) => {
 
     // Number of tickets
     console.log('First page => Tickets')
+    await test.step('Verify addition of tickets', async () => { 
+    });
     await expect(NewTicket.formTicket).toBeVisible; // assert if we are on the ticket
     await NewTicket.addTicket.click();    // add one ticket
     await delay(5000);
@@ -30,6 +35,8 @@ test('Buying Billetto Tickets',async ({page}) => {
 
    // Capture Buyer Details
     await delay(10000);
+    await test.step('Verify capturing and submission of Buyer  Details', async () => { 
+    });
     await expect(BuyerDetails.formBuyerDetails).toBeVisible;// assert if we are on Buyer Details
     await BuyerDetails.attendeeName.fill('Tata Karabo');
     await BuyerDetails.buyerEmail.fill('tom@healthtower.store');
@@ -51,20 +58,27 @@ test('Buying Billetto Tickets',async ({page}) => {
   
     // Services
     await delay(5000);
+    await test.step('Verify selection of Services', async () => { 
+    });
     await Ticket_Services.serviceOne.click();
     await Ticket_Services.serviceTwo.click();
     await Ticket_Services.serviceThree.click();
     await Ticket_Services.nextPayment.click();//click Next => Payment
+  
     console.log('Next page => Payment')
 
 
     // Payments
     await expect(selectPayment.formPayment).toBeVisible;
     await selectPayment.submitPaymentMethod.click();
+ 
     await delay(1000);
 
 
     // Create the screenshots folder if it doesn't exist
+    await test.step('Verify submission of payment method', async () => { 
+    });
+
     const folderPath = './screenshots';
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
@@ -80,6 +94,13 @@ test('Buying Billetto Tickets',async ({page}) => {
 
     
 test('Submitting Payment',async ({page}) => {
+  
+    await test.step('Verify Method of Payment Selected', async () => {
+      
+    });
+  
+    await test.step('Verify URL for payment', async () => {
+      
+    });
+  });
 
-
-});
